f4767f8e9788f83775c982e3e422c59df1dc37e8
Jaymin Makwana <69162579+jamu1570@users.noreply.github.com>
2020-12-26 23:00:58 +0530

    FAQ page done

--- js/script.js
+++ js/script.js
@@ -37,3 +37,31 @@ $("#toggle-password3").click(function() {
     x.type = "password";
   }
 });
+
+/*==================================================
+                    FAQ
+==================================================*/
+
+//faq toggle stuff
+$(".togglefaq").click(function(e) {
+  e.preventDefault();
+  var notthis = $(".active").not(this);
+  notthis
+    .find(".fa-minus")
+    .addClass("fa fa-plus")
+    .removeClass("fa fa-minus");
+  notthis
+    .toggleClass("active")
+    .next(".faqanswer")
+    .slideToggle(300);
+  $(this)
+    .toggleClass("active")
+    .next()
+    .slideToggle("fast");
+  // $(this)
+  //   .children("i")
+  //   .toggleClass("fa-plus fa-minus");
+  $(this)
+    .find("i")
+    .toggleClass("fa-plus fa-minus");
+});
