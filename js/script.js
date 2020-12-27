6512e27d77e9d964caff0fd19dfb41359c59206a
Jaymin Makwana <69162579+jamu1570@users.noreply.github.com>
2020-12-27 13:47:16 +0530

    header added

--- js/script.js
+++ js/script.js
@@ -48,8 +48,8 @@ $(".togglefaq").click(function(e) {
   var notthis = $(".active").not(this);
   notthis
     .find(".fa-minus")
-    .addClass("fa fa-plus")
-    .removeClass("fa fa-minus");
+    .addClass("fa-plus")
+    .removeClass("fa-minus");
   notthis
     .toggleClass("active")
     .next(".faqanswer")
@@ -58,9 +58,6 @@ $(".togglefaq").click(function(e) {
     .toggleClass("active")
     .next()
     .slideToggle("fast");
-  // $(this)
-  //   .children("i")
-  //   .toggleClass("fa-plus fa-minus");
   $(this)
     .find("i")
     .toggleClass("fa-plus fa-minus");
