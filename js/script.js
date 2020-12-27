b0ab45a8d5ff2f2d12fea848efb75a7b0d7f514f
Jaymin Makwana <69162579+jamu1570@users.noreply.github.com>
2020-12-27 22:07:03 +0530

    all page done

--- js/script.js
+++ js/script.js
@@ -62,3 +62,39 @@ $(".togglefaq").click(function(e) {
     .find("i")
     .toggleClass("fa-plus fa-minus");
 });
+
+/*==================================================
+                    Navbar
+==================================================*/
+$(function() {
+  //Show/Hide nav on page load
+  ShowHideNav();
+
+  $(window).scroll(function() {
+    //Show/Hide nav on window's Scroll
+    ShowHideNav();
+  });
+
+  function ShowHideNav() {
+    if ($(window).scrollTop() > 50) {
+      // Show white nav
+      $("header").removeClass("navbar-transparent");
+
+      // Show dark logo
+      $(".navbar-brand-2 img").attr("src", "images/Login/top-logo_2.png");
+    } else {
+      // Hide white nav
+      $("header").addClass("navbar-transparent");
+
+      // Show dark logo
+      $(".navbar-brand-2 img").attr("src", "images/Login/top-logo.png");
+    }
+    if ($(window).width() < 768) {
+      $("header").removeClass("navbar-transparent");
+      // Show dark logo
+      $(".navbar-brand img").attr("src", "images/Login/top-logo_2.png");
+    } else {
+      //
+    }
+  }
+});
